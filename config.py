5dc8b8c4c58c309568c1a244e47c3c19ad38d102
minhalo113 <115437688+minhalo113@users.noreply.github.com>
2024-08-03 19:00:33 -0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-API_YTB_KEY = "AIzaSyBzqVzDsoc5Hu3F6iT3T-s6qQAkX6EBuJQ" 
-API_HUGGINGFACE_KEY = "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"
-API_WORDPRESS = "5Za7 tABN K4LA Bvsj SlLZ MO2N"
-EMAIL_WORDPRESS = 'minhalo113@gmail.com'
-WEB_URL = "https://ahistoryfactaday.org/wp-json/wp/v2"
-
-
-YTB_CHANNEL_ID1 = "UCNAek4srjvZvjptytiH99bA"
